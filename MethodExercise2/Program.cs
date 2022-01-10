1a813f41e02f0a194c0d0fc7f2c001bf1e52939a
Gleb Dudarev <glebdydarev@mail.ru>
2022-01-10 16:35:03 -0600

    message

---
+++ MethodExercise2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MethodExercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            var name = Console.ReadLine();
+
+            var result = Add(10, 20);
+
+            Console.WriteLine(result);
+
+            var result2 = Multiply(30, 50);
+
+            Console.WriteLine(result2);
+        }
+        public static int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+
+        public static int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+
+        public static int Substract(int num1, int num2)
+        {
+            return num1 - num2;
+
+        }
+        public static int Divide (int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        public static int Modulus (int num1, int num2)
+        {
+            return num1 % num2;  
+        }
+        
+    }
+}
